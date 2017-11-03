fdba7eed5a241c7e6d1b9df4b27e79a7548b9193
Meera <mjvsharma@outlook.com>
2017-11-03 15:23:21 -0400

    mapped object with methods in json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,35 @@
 class BooksController < ApplicationController
-def index
+  def index
+  @books = Book.all
 
   respond_to do |format|
     format.html
+    format.text
+
+    format.csv do
+        render plain: Book.generate_csv(@books)
+    end
+
+
+    format.json do
+      render json: @books.map { |book|
+        {
+          title: book.title,
+          author: book.author,
+          already_read: book.already_read
+
+        }
+
+      }
+        
+      end
+
+
+
+
+
   end
+
 end
 
 
